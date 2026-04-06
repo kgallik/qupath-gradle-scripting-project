b4fb34768d72d3fd7be93aab11972da79dccb3c1
KGallik <kristin.gallik@gmail.com>
2026-04-06 17:56:16 -0400

    Added better approach for creating smooth boundaries

--- scripts/Expand Nuclear Detection Add Measurements.groovy
+++ scripts/Expand Nuclear Detection Add Measurements.groovy
@@ -1,13 +1,21 @@
 import qupath.lib.analysis.features.ObjectMeasurements
-def serverOriginal = getCurrentServer()
-param_expansion = 5
+//def serverOriginal = getCurrentServer()
+double paramExpansion = 2
+double nucScale = -1
 img_resolution=getCurrentImageData().getServer().getPixelCalibration().getAveragedPixelSizeMicrons() //Get the current image's resolution
 def detections = getDetectionObjects()
-def cells = CellTools.detectionsToCells(detections, param_expansion/img_resolution, -1)
-clearCellMeasurements()
-ObjectMeasurements.addShapeMeasurements(cells, serverOriginal.getPixelCalibration())
-cells.each(p -> ObjectMeasurements.addIntensityMeasurements(serverOriginal, p, 1.0, 
-                            ObjectMeasurements.Measurements.values() as List,
-                            ObjectMeasurements.Compartments.values() as List))
+def cellBoundaries = [:]
+detections.each {it ->
+    def roi = it.getROI()
+    def geom = roi.getGeometry()
+    def boundary = CellTools.estimateCellBoundary(geom,paramExpansion/img_resolution,nucScale)
+    cellBoundaries.put(it,boundary)
+}
+def cells = CellTools.detectionsToCells(cellBoundaries)
+//clearCellMeasurements()
+//ObjectMeasurements.addShapeMeasurements(cells, serverOriginal.getPixelCalibration())
+//cells.each(p -> ObjectMeasurements.addIntensityMeasurements(serverOriginal, p, 1.0, 
+//                            ObjectMeasurements.Measurements.values() as List,
+//                            ObjectMeasurements.Compartments.values() as List))
 removeObjects(detections, true)
 addObjects(cells)
